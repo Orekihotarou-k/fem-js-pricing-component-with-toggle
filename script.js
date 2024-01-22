b23b443ad34a44bb247669496dadcdc9b424df44
Levis Kim <122298089+Orekihotarou-k@users.noreply.github.com>
2024-01-22 16:48:30 +0100

    no commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const priceToggle = document.querySelector('.price-toggle')
+const cardAmount = document.querySelector('.card-amount')
+
+priceToggle.addEventListener("click", () => {
+    cardAmount.
+})
