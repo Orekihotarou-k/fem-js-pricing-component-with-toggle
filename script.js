180360856ba8eb62221dfcf64b6b39714541d8bd
Levis Kim <122298089+Orekihotarou-k@users.noreply.github.com>
2024-01-22 16:48:54 +0100

    Revert "no commit"
    This reverts commit b23b443ad34a44bb247669496dadcdc9b424df44.

--- script.js
+++
@@ -1,6 +0,0 @@
-const priceToggle = document.querySelector('.price-toggle')
-const cardAmount = document.querySelector('.card-amount')
-
-priceToggle.addEventListener("click", () => {
-    cardAmount.
-})
